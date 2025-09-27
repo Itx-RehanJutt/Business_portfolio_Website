7a2ed0d606e8df864fc6981575f8d7808e2f64e7
Sherazasmat <sherazasmat435@gmail.com>
2025-09-27 21:48:58 +0500

    feat: Add social login icons and update image imports in Login component

--- src/Dashboard/PortfolioDashboard.jsx
+++ src/Dashboard/PortfolioDashboard.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import Profile from "../assets/profile.png";
 import Message from "../assets/message.png";
-import Delete from "../assets/bin.png";
-import Edit from "../assets/editbtn.png";
+import Delete from "../assets/delete.png";
+import Edit from "../assets/edit.png";
 
 function PortfolioDashboard() {
   return (
-    <div className="min-h-screen bg-[#C7C6FB] py-8 px-4 flex flex-col justify-center items-center">
+    <div className="min-h-screen bg-[#C7C6FB] w-full py-8 px-0 flex flex-col items-center">
       {/* Title Section */}
-      <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-center text-white mt-4 mb-24">
+      <h2 className="text-2xl sm:text-3xl sm:text-center md:text-4xl lg:text-5xl font-semibold text-center text-white mt-4 mb-12">
         Profile Management
       </h2>
 
       {/* Profile Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+      <div className=" w-full max-w-7xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 sm:gap-19 md:gap-139 px-4">
         {/* Profile Card */}
-        <div className="bg-white pt-10 pb-6 items-center rounded-xl shadow-lg hover:shadow-2xl transition-all duration-300 transform hover:scale-105">
+        <div className=" w-135 bg-white pt-10 pb-2 rounded-xl shadow-lg hover:shadow-2xl transition-all duration-300 transform hover:scale-105">
           <img
             src={Profile}
             alt="Profile"
             className="w-full pb-5 object-cover rounded-lg"
           />
-          <div className="mt-4">
-            <div className="mt-4 flex flex-wrap justify-center gap-3 sm:gap-4 md:gap-5 sm:flex-row">
+          <div className="m-4">
+            <div className="mt-4 flex flex-wrap justify-center gap-6 sm:gap-1 md:gap-8 sm:flex-row">
               {/* Buttons */}
-              <button className="bg-green-500 text-white font-semibold px-4 py-1 rounded-md transition duration-300 hover:bg-green-600 flex items-center space-x-2">
+              <button className="bg-green-500 text-white font-semibold px-2 py-2 rounded-md transition duration-300 hover:bg-green-600 flex items-center space-x-2">
                 <span>Edit</span>
                 <img src={Edit} alt="edit" />
               </button>
@@ -33,7 +33,7 @@ function PortfolioDashboard() {
                 <img src={Message} alt="message" />
               </button>
 
-              <button className="text-white rounded-md transition duration-300 hover:bg-red-200 flex items-center space-x-2">
+              <button className="text-white px-2 py-2 sm:px-3 sm:py-1 text-lg sm:text-sm rounded-md transition duration-300 hover:bg-gray-200 flex items-center space-x-2">
                 <img src={Delete} alt="delete" />
               </button>
 
@@ -45,29 +45,29 @@ function PortfolioDashboard() {
         </div>
 
         {/* Duplicate the Profile Cards for Demo */}
-        <div className="bg-white pt-10 pb-6 rounded-xl shadow-lg hover:shadow-2xl transition-all duration-300 transform hover:scale-105">
+        <div className=" w-135 bg-white pt-10 pb-6 rounded-xl shadow-lg hover:shadow-2xl transition-all duration-300 transform hover:scale-105">
           <img
             src={Profile}
             alt="Profile"
             className="w-full pb-5 object-cover rounded-lg"
           />
           <div className="mt-4">
-            <div className="mt-4 flex flex-wrap justify-center gap-3 sm:gap-4 md:gap-7 sm:flex-row">
+            <div className="mt-4 flex flex-wrap justify-center gap-6 sm:gap-1 md:gap-8 sm:flex-row">
               {/* Buttons */}
-              <button className="bg-green-500 text-white font-semibold px-4 py-1 rounded-md transition duration-300 hover:bg-green-600 flex items-center space-x-2">
+              <button className="bg-green-500 text-white font-semibold px-2 py-2 sm:px-3 sm:py-1 text-lg sm:text-sm rounded-md transition duration-300 hover:bg-green-600 flex items-center space-x-2">
                 <span>Edit</span>
                 <img src={Edit} alt="edit" />
               </button>
 
-              <button className="bg-yellow-500 text-white px-3 py-1 rounded-md transition duration-300 hover:bg-yellow-600 flex items-center space-x-2">
+              <button className="bg-yellow-500 text-white px-4 py-2 sm:px-3 sm:py-1 text-lg sm:text-sm rounded-md transition duration-300 hover:bg-yellow-600 flex items-center space-x-2">
                 <img src={Message} alt="message" />
               </button>
 
-              <button className="text-white rounded-md transition duration-300 hover:bg-black-50 hover:bg-red-200 flex items-center space-x-2">
+              <button className="text-white px-2 py-2 sm:px-3 sm:py-1 text-lg sm:text-sm rounded-md transition duration-300 hover:bg-gray-200 flex items-center space-x-2">
                 <img src={Delete} alt="delete" />
               </button>
 
-              <button className="bg-gray-400 px-4 py-1 font-semibold text-white rounded-md transition duration-300 hover:bg-gray-500 flex items-center space-x-2">
+              <button className="bg-gray-400 px-4 py-2 sm:px-3 sm:py-1 text-lg sm:text-sm font-semibold text-white rounded-md transition duration-300 hover:bg-gray-500 flex items-center space-x-2">
                 <span>View</span>
               </button>
             </div>
